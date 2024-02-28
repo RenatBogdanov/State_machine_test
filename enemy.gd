0bd24ab0d172478723e3acddcfb956c60695f784
Renat Bogdanov <zeloniyosminog@gmail.com>
2024-02-28 23:39:26 +0300

    Перемещение в разные стороны
    Реализация случайного перемещения в вправо/влево с поворотом спрайта

--- enemy.gd
+++ enemy.gd
@@ -12,17 +12,21 @@ func _ready():
 
 func _physics_process(delta):
 	var velocity = Vector2.ZERO
-	if isRun and (position.x < 1000):
+	if isRun == 1 and (position.x < 1000):
 		$AnimatedSprite2D.play("run")
 		velocity.x += 1
-	elif isRun and (position.x >= 1000):
-		$AnimatedSprite2D.play("idle")
-	
+	elif isRun == -1 and (position.x > -1000):
+		$AnimatedSprite2D.play("run")
+		velocity.x -= 1
 	velocity = velocity.normalized() * SPEED
 	position += velocity * delta
 
-func _on_run_state_is_run(check: bool):
-	if check:
+func _on_run_state_is_run(direction: String):
+	if direction == "right":
 		isRun = 1
-	else:
+		$AnimatedSprite2D.flip_h = false
+	elif direction == "left":
+		isRun = -1
+		$AnimatedSprite2D.flip_h = true
+	elif direction == "stay":
 		isRun = 0
--- scripts/RunState.gd
+++ scripts/RunState.gd
@@ -1,18 +1,23 @@
 extends Node
 
 var fsm: StateMachine
-signal isRun(check: bool)
+signal isRun(direction: String)
 
 func enter():
 	print("Run")
-	isRun.emit(true)
+	isRun.emit("stay")
 	# Exit 2 seconds later
 	await get_tree().create_timer(0.2).timeout
-	isRun.emit(false)
+	if randf() > 0.5:
+		isRun.emit("right")
+	else:
+		isRun.emit("left")
+	await get_tree().create_timer(0.2).timeout
 	exit("IdleState")
 
 func exit(next_state):
 	fsm.change_to(next_state)
+	isRun.emit("stay")
 
 
 func _unhandled_key_input(event):
